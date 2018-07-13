1edc06c892a55d292d3b33ae5f62d7b71e12d89d
Aleksandr Demakin <a.demakin@space307.com>
2018-07-13 16:26:35 +0300

    machine/atkins-diet: free spin tests.

--- machine/atkins-diet/atkins_test.go
+++ machine/atkins-diet/atkins_test.go
@@ -69,8 +69,7 @@ func TestCheckPayLine(t *testing.T) {
 	a.Equal(expected, actual)
 }
 
-func TestDoMain(t *testing.T) {
-	a := assert.New(t)
+func makeTestConfig() Config {
 	config := Config{
 		Wild:    10,
 		Scatter: 11,
@@ -90,8 +89,14 @@ func TestDoMain(t *testing.T) {
 		l := Line{0, 0, 1, 2, 3}
 		config.Pays[i] = l
 	}
+	return config
+}
+
+func TestDoMain(t *testing.T) {
+	a := assert.New(t)
+	config := makeTestConfig()
 	tls := &testLineSource{data: Line{0, 0, 0, 0, 0}}
-	m := New(config, tls)
+	m, _ := New(config, tls)
 	res, free := m.doMain(1)
 	a.Equal(machine.SpinResult{Type: "main", Total: 3, Stops: tls.data[:]}, res)
 	a.False(free)
@@ -126,4 +131,84 @@ func TestDoMain(t *testing.T) {
 	res, free = m.doMain(2)
 	a.Equal(machine.SpinResult{Type: "main", Total: 6, Stops: tls.data[:]}, res)
 	a.True(free)
+}
+
+type testCountedLineSource struct {
+	count int
+	f     func(int) Line
+}
+
+func (ts *testCountedLineSource) Line() Line {
+	result := ts.f(ts.count)
+	ts.count++
+	return result
+}
+
+func TestDoFree(t *testing.T) {
+	a := assert.New(t)
+	config := makeTestConfig()
+	tls := &testCountedLineSource{
+		f: func(count int) Line {
+			if count == 5 {
+				return Line{10, 10, 10, 10, 10}
+			}
+			return Line{1, 1, 1, 1, 1}
+		},
+	}
+	m, _ := New(config, tls)
+
+	totalFunc := func(res []machine.SpinResult) (sum int64) {
+		for _, r := range res {
+			sum += r.Total
+		}
+		return
+	}
+
+	res := m.doFree(1)
+	a.Equal(20, len(res))
+	a.Equal(int64(360), totalFunc(res))
+
+	tls.count = 0
+	tls.f = func(count int) Line {
+		if count == 5 || count == 7 {
+			return Line{11, 11, 11, 11, 11}
+		}
+		return Line{1, 1, 1, 1, 1}
+	}
+	res = m.doFree(1)
+	a.Equal(30, len(res))
+	a.Equal(int64(648), totalFunc(res))
+
+	tls.count = 0
+	tls.f = func(count int) Line {
+		if count == 5 {
+			return Line{11, 11, 11, 11, 11}
+		}
+		return Line{1, 1, 1, 1, 1}
+	}
+	res = m.doFree(1)
+	a.Equal(20, len(res))
+	a.Equal(int64(369), totalFunc(res))
+
+	tls.count = 0
+	tls.f = func(count int) Line {
+		if count == 5 || count == 15 {
+			return Line{10, 10, 10, 10, 10}
+		}
+		return Line{1, 1, 1, 1, 1}
+	}
+	res = m.doFree(1)
+	a.Equal(30, len(res))
+	a.Equal(int64(1170), totalFunc(res))
+
+	tls.count = 0
+	tls.f = func(count int) Line {
+		if count == 5 || count == 15 || count == 25 {
+			return Line{10, 10, 10, 10, 10}
+		}
+		return Line{1, 1, 1, 1, 1}
+	}
+	res = m.doFree(1)
+	a.Equal(40, len(res))
+	a.Equal(int64(3600), totalFunc(res))
 }
